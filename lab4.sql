9c2967e3c1460a335ac20f6882ac4ec935e5d6ad
demo student <weixuebingcp@gmail.com>
2024-09-23 17:44:32 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -2,5 +2,28 @@
 /* no, because we still have VA records in other tables */
 
 --Q4.2
-
 /*no, because no state has  fips of 80*/
+
+--Q4.3
+/* it works, but can be improved */ 
+
+--4.5
+/* yes, great job*/ 
+
+--4.6
+SELECT n.name, i.income, i.year
+FROM income i
+JOIN name n ON i.fips = n.fips
+WHERE i.year = (
+    -- Get the most recent year
+    SELECT MAX(year) 
+    FROM income
+)
+ORDER BY i.income DESC
+LIMIT 1;
+/* yes, it works */
+
+
+--4.6
+
+/* it doens't work */
